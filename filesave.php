2ce807c4746eb5d7387599a0a10b571bacaf4a3d
Dave Wong <dave@dhmw.net>
2015-04-27 14:09:33 +1000

    Add json response from filesave php

--- filesave.php
+++ filesave.php
@@ -1,14 +1,28 @@
 <?php
 
-// Source: http://stackoverflow.com/questions/10955017/sending-json-to-php-using-ajax
+/*
+  Sources: 
+  http://stackoverflow.com/questions/10955017/sending-json-to-php-using-ajax
+  http://www.w3schools.com/php/php_file_create.asp
+  http://stackoverflow.com/questions/3921520/writing-json-object-to-json-file-on-server
+
+*/
+$saved_file = "saved_register.json";
+
 if(isset($_POST['regData'])){
-$obj = $_POST['regData'];
+  $obj = $_POST['regData'];
 
- //Source: http://www.w3schools.com/php/php_file_create.asp
-$myfile = fopen("saved_register.json", "w") or die("Unable to open file!");
-fwrite($myfile, $obj);
-fclose($myfile);
+  if(json_decode($obj) != null) {
+    $myfile = fopen($saved_file, "w") or die("Unable to open file!");
+    fwrite($myfile, $obj);
+    fclose($myfile);  
+  } else {
+    // emit error, $obj was stringified JSON.
+  }
 
+} else if( file_exists( $saved_file ) ) {
+  
+  echo file_get_contents($saved_file);
 }
 
 ?>
